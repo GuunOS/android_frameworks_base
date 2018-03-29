a62aae798fda7034d631c7f0da4f4af704d940c4
Psycho-Mods <nishsin26@gmail.com>
2018-03-29 13:46:50 +0530

    Revert "Try Fixing"
    This reverts commit 2db39bd7f15eb890304d7c0bb34b0d96967590d2.

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -24,7 +24,6 @@ import android.os.Handler;
 import android.os.Message;
 import android.os.UserHandle;
 import android.provider.Settings;
-import android.provider.Settings.Secure;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
