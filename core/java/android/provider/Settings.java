95578feec8227bc23185ce1ef1395da8c893bf67
Psycho-Mods <nishsin26@gmail.com>
2018-07-06 19:47:55 +0530

    Fix Derps

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3770,6 +3770,12 @@ public final class Settings {
         public static final String DOZE_SCREEN_BRIGHTNESS = "doze_screen_brightness";
 
         /**
+         * Change the screenshot delay
+         * @hide
+         */
+        public static final String SCREENSHOT_DELAY = "screenshot_delay";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
